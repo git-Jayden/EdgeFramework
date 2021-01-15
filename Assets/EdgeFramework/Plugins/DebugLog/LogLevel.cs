ebab4eae19295baee5257b261bed92e1098177a4
Jayden <w_style047@163.com>
2021-01-15 15:02:25 +0800

    Debug

---
+++ Assets/EdgeFramework/Plugins/DebugLog/LogLevel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ls_Mobile
+{
+    public enum LogLevel
+    {
+        None,
+        Exception,
+        Error,
+        Warning,
+        Normal,
+        Max
+    }
+}
