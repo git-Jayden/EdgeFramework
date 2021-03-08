e256c7ffa07a6022dc058489f054479399c16bf5
Jayden <w_style047@163.com>
2021-03-08 17:23:07 +0800

    Test Script

---
+++ HotfixProject/Hotfix/TestMono.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+namespace Hotfix
+{
+   public class TestMono
+    {
+        public static void RunTest(GameObject go)
+        {
+            go.AddComponent<MonoTest>();
+        }
+        public static void RunTest1(GameObject go)
+        {
+            go.AddComponent<MonoTest>();
+            MonoTest mono = go.GetComponent<MonoTest>();
+            mono.Test();
+        }
+    }
+    public class MonoTest : MonoBehaviour
+    {
+        private float mCurTime = 0;
+        void Awake()
+        {
+            Debug.Log("MonoTest Awake");
+        }
+        void Start()
+        {
+            Debug.Log("MonoTest Start!");
+        }
+        void Update()
+        {
+            if (mCurTime < 0.2f)
+            {
+                mCurTime += Time.deltaTime;
+                Debug.Log("MonoTest Update!");
+            }
+        }
+        public  void Test()
+        {
+            Debug.Log("MonoTest");
+        }
+    }
+}
