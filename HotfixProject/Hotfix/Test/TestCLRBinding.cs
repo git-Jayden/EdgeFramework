0ac17500a23a82f55b71f5332db36618e1d15993
Jayden <w_style047@163.com>
2021-03-13 16:00:45 +0800

    Add HotfixCode

---
+++ HotfixProject/Hotfix/Test/TestCLRBinding.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotfix
+{
+    public  class TestCLRBinding
+    {
+        public static void RunTest()
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                CRLBindingTestClass.DoSomeTest(i,i);
+            }
+        }
+    }
+}
---
+++ HotfixProject/Hotfix/Test/TestCortoutine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Hotfix
+{
+   public  class TestCortoutine
+    {
+        public static void RunTest()
+        {
+            GameRoot.Instance.StartCoroutine(Coroutine());
+        }
+
+        static System.Collections.IEnumerator Coroutine()
+        {
+            Debug.Log("开始协成,t=" + Time.time);
+            yield return new WaitForSeconds(3);
+            Debug.Log("开始完成,t=" + Time.time);
+
+        }
+    }
+}
